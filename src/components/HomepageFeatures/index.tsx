2dde1f5523dad90cf05ce19047c7183cedffe336
CertGym <certgym.com@gmail.com>
2026-08-16 18:59:16 +0800

    feat: update wording

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -25,7 +25,8 @@ const FeatureList: FeatureItem[] = [
     icon: "📝",
     description: (
       <>
-        涵蓋會計師、領隊、導遊等國家考試，以及金融證照、iPAS AI、PMP、AWS
+        涵蓋公職、法律、醫護、財經會計、領隊、導遊等國家考試，以及金融證照、iPAS
+        AI、PMP、AWS
         等熱門證照題庫，支援模擬測驗與即時解析，練習更有效率，快速掌握考點規律。
       </>
     ),
